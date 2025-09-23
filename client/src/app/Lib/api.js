2386922f1472bb551f70cb5b7204a508bd4b55a6
AkumuOmolo <akumu.omoloh@gmail.com>
2025-09-23 13:13:59 +0300

    Adds patch delete update station api fetch

--- client/src/app/Lib/api.js
+++ client/src/app/Lib/api.js
@@ -24,3 +24,22 @@ export async function addStation(station) {
     return await res.json();
 }
 
+// Update Station
+export async function updateStation(id, updates) {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(updates),
+    });
+    if (!res.ok) throw new Error("Failed to update station");
+    return await res.json();
+}
+
+// Delete Station
+export async function deleteStation(id) {
+    const res = await fetch(`${BASE_URL}/${id}`, {
+        method:"DELETE"
+    });
+    if (!res.ok) throw new Error("Failed to delete station");
+    return true;
+}
