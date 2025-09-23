b4885921c535c1b561b6aa686f4677fd01bc3ec5
Jeremy Marube <jeremymarube1@gmail.com>
2025-09-23 12:32:24 +0300

    Refactor: Updating Pump model structure and relationships

--- server/models.py
+++ server/models.py
@@ -1,38 +1,18 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
-from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-from config import db
-
-# Models go here!
 db = SQLAlchemy()
 
+class Pump(db.Model):
+    __tablename__ = "pumps"
 
-class Pump(db.Model, SerializerMixin):
-    __tablename__ = 'pumps'
-    
     id = db.Column(db.Integer, primary_key=True)
-    pump_number = db.Column(db.String(10), nullable=False)
-    fuel_type = db.Column(db.String(50), nullable=False)
-    
-    # Relationships
-    sales = db.relationship('Sale', backref='pump', cascade='all, delete-orphan')
-    
-    serialize_rules = ('-sales.pump',)
+    pump_number = db.Column(db.String(50), nullable=False, unique=True)
 
-class Sale(db.Model, SerializerMixin):
-    __tablename__ = 'sales'
-    
-    id = db.Column(db.Integer, primary_key=True)
-    fuel_type = db.Column(db.String(50), nullable=False)
-    litres = db.Column(db.Float, nullable=False)
-    price_per_litre = db.Column(db.Float, nullable=False)
-    total_amount = db.Column(db.Float, nullable=False)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-    pump_id = db.Column(db.Integer, db.ForeignKey('pumps.id'), nullable=False)
-    
-    serialize_rules = ('-pump.sales',)
+    sales = db.relationship("Sale", backref="pump", lazy=True)
 
-    def __repr__(self):
-        return f'<Sale {self.fuel_type} - {self.litres}L>'
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "pump_number": self.pump_number
+        }
